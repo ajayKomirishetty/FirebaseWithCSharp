d44a350eb1784e2c2e1d3ca8f59bd9386c74ec78
Ajay Komirishetty <ajay.komirishetty@ggktech.local>
2018-03-28 18:05:46 +0530

    initial commit

---
+++ FireSharpTutorial/FireSharpTutorial/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace FireSharpTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var json = JsonConvert.SerializeObject(new { name = "sumanth", value = DateTime.Now ,});
+           var request= WebRequest.CreateHttp("https://fir-practice-7a311.firebaseio.com/.json");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer,0,buffer.Length);
+            var response = request.GetResponse();
+            json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+        
+            InitializeComponent();
+        }
+    }
+}
